70d6344c05771253215f6276327e1431e4155ec2
brunomaria71 <brunomaria71@yahoo.com>
2022-04-29 11:16:37 -0400

    added a collection named restaurant

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const admin = require("firebase-admin");
 
 const serviceAccount = require('./credentials.json');
@@ -6,3 +5,18 @@ const serviceAccount = require('./credentials.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+ const db = admin.firestore();
+
+
+ const restaurant = {
+     name: 'Mister 01',
+     address: '555 N Federal',
+     cuisine: 'Pizza',
+     rating: 4.9,
+     phone: '786-677-2903'
+ }
+
+ db.collection('restaurants').add(restaurant)
+    .then(doc => console.log('created restaurant', doc.id))
+    .catch(err => console.error(err))
