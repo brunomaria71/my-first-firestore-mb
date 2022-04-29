2b5533c94c286aba9256b71c8d34c1bec141cebd
brunomaria71 <brunomaria71@yahoo.com>
2022-04-29 11:33:48 -0400

    adding data bolay

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ admin.initializeApp({
      phone: '786-677-2903'
  }
 
- db.collection('restaurants').add(restaurant)
-    .then(doc => console.log('created restaurant', doc.id))
-    .catch(err => console.error(err))
+//  db.collection('restaurants').add(restaurant)
+//     .then(doc => console.log('created restaurant', doc.id))
+//     .catch(err => console.error(err))
+
+const restaurant2 = {
+    name: "Bolay",
+    address: "7060 West Palmetto Rd",
+    cuisine: "American",
+    rating: 4.6,
+}
+
+async function addRestauarant(data){
+    try {
+    const doc = await db.collection('restaurants').add(restaurant2)
+    console.log('Created restaruant', doc.id)
+    } catch(err) {
+        console.error(err)
+    }
+} 
+
+addRestauarant(restaurant2)
